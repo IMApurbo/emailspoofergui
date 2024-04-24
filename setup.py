ab9c726d426f810323fdc0c31e1488eb7f73365b
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-04-24 14:57:31 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='emailspoofergui',
-    version='3.1',
+    version='3.2',
     packages=find_packages(),
     package_data={'emailspoofergui': ['data/config.json']},
     install_requires=[
