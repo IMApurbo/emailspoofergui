eb435dda9b1ee3bdee377dc4348c12896630e5ba
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-12-13 15:41:34 +0600

    Update emailspoofergui.py

--- emailspoofergui/emailspoofergui.py
+++ emailspoofergui/emailspoofergui.py
@@ -8,16 +8,16 @@ from tkinter import filedialog
 
 
 def send_email():
-    # Function to send the email
     def send():
-        sender_name = sender_entry.get()
+        sender_name = sender_name_entry.get()  # Get sender's name
+        sender_email = sender_email_entry.get()  # Get sender's email
         receiver_email = receiver_entry.get()
         subject = subject_entry.get()
         message = message_entry.get("1.0", "end-1c")
         attachment = attachment_entry.get()
         tls = tls_var.get()
 
-        sendemail_command = f"sendemail -xu {config['username']} -xp {config['password']} -s {config['smtp_server']} -o tls={tls} -f '{sender_name}' -t {receiver_email} -u '{subject}' -m '{message}'"
+        sendemail_command = f"sendemail -xu {config['username']} -xp {config['password']} -s {config['smtp_server']} -o tls={tls} -f '{sender_name} <{sender_email}>' -t {receiver_email} -u '{subject}' -m '{message}'"
         
         if attachment:
             sendemail_command += f" -a '{attachment}'"
@@ -75,66 +75,58 @@ def send_email():
     config = load_config()
 
     if config:
-        
-        # Create a label with text "KORISHEE THE CYBERMASTER"
-        label = ctk.CTkLabel(root, text="KORISHEE THE CYBERMASTER", font=("Comic Sans MS", 18 , "bold"))
-        # Use sticky option to center the label horizontally
+        label = ctk.CTkLabel(root, text="KORISHEE THE CYBERMASTER", font=("Comic Sans MS", 18, "bold"))
         label.grid(row=0, column=0, columnspan=2, pady=10, sticky="ew")
-            
-        sender_label = ctk.CTkLabel(root, text="Sender's Email:  ",font=("Latin Modern Mono Slanted", 16))
-        sender_label.grid(row=1, column=0, sticky="w")
-        
-        sender_entry = ctk.CTkEntry(root, width=600,border_color="blue",font=("Latin Modern Mono Slanted", 16))
-        sender_entry.grid(row=1, column=1, sticky="e")
-    
-        receiver_label = ctk.CTkLabel(root, text="Recipient's Email:  ",font=("Latin Modern Mono Slanted", 16))
-        receiver_label.grid(row=2, column=0, sticky="w")
 
-        receiver_entry = ctk.CTkEntry(root, width=600,border_color="red",font=("Latin Modern Mono Slanted", 16))
-        receiver_entry.grid(row=2, column=1, sticky="e")
+        sender_name_label = ctk.CTkLabel(root, text="Sender's Name:  ", font=("Latin Modern Mono Slanted", 16))
+        sender_name_label.grid(row=1, column=0, sticky="w")
+
+        sender_name_entry = ctk.CTkEntry(root, width=600, border_color="red", font=("Latin Modern Mono Slanted", 16))
+        sender_name_entry.grid(row=1, column=1, sticky="e")
+
+        sender_email_label = ctk.CTkLabel(root, text="Sender's Email:  ", font=("Latin Modern Mono Slanted", 16))
+        sender_email_label.grid(row=2, column=0, sticky="w")
+
+        sender_email_entry = ctk.CTkEntry(root, width=600, border_color="blue", font=("Latin Modern Mono Slanted", 16))
+        sender_email_entry.grid(row=2, column=1, sticky="e")
+
+        receiver_label = ctk.CTkLabel(root, text="Recipient's Email:  ", font=("Latin Modern Mono Slanted", 16))
+        receiver_label.grid(row=3, column=0, sticky="w")
+
+        receiver_entry = ctk.CTkEntry(root, width=600, border_color="red", font=("Latin Modern Mono Slanted", 16))
+        receiver_entry.grid(row=3, column=1, sticky="e")
 
         subject_label = ctk.CTkLabel(root, text="Subject:  ", font=("Latin Modern Mono Slanted", 16))
-        subject_label.grid(row=3, column=0, sticky="w")
+        subject_label.grid(row=4, column=0, sticky="w")
 
-        subject_entry = ctk.CTkEntry(root, width=600,border_color="blue",font=("Latin Modern Mono Slanted", 16))
-        subject_entry.grid(row=3, column=1, sticky="e")
+        subject_entry = ctk.CTkEntry(root, width=600, border_color="blue", font=("Latin Modern Mono Slanted", 16))
+        subject_entry.grid(row=4, column=1, sticky="e")
 
-        message_label = ctk.CTkLabel(root, text="Message:  ",font=("Latin Modern Mono Slanted", 16))
-        message_label.grid(row=4, column=0, sticky="w")
+        message_label = ctk.CTkLabel(root, text="Message:  ", font=("Latin Modern Mono Slanted", 16))
+        message_label.grid(row=5, column=0, sticky="w")
 
-        message_entry = ctk.CTkTextbox(root, width=600, height=250,font=("TeX Gyre Pagella", 14,"bold"))
-        message_entry.grid(row=4, column=1, sticky="e")
+        message_entry = ctk.CTkTextbox(root, width=600, height=250, font=("TeX Gyre Pagella", 14, "bold"))
+        message_entry.grid(row=5, column=1, sticky="e")
 
-        # Add attachment entry and button
         attachment_label = ctk.CTkLabel(root, text="Attachment: ", font=("Latin Modern Mono Slanted", 16))
-        attachment_label.grid(row=5, column=0, sticky="w")
-        
-        attachment_entry = ctk.CTkEntry(root, width=500, border_color="blue",text_color="red", font=("Latin Modern Mono Slanted", 16))
-        attachment_entry.grid(row=5, column=1, sticky="w")
+        attachment_label.grid(row=6, column=0, sticky="w")
 
-        attachment_button = ctk.CTkButton(root,width=100,border_color="red",hover_color="black", border_width=3,fg_color="blue", text="📎", command=select_attachment)
-        attachment_button.grid(row=5, column=1, sticky="e")
-        
-        send_button = ctk.CTkButton(root, text="Send Email",font=("MathJax_Main", 16,"italic"),fg_color="blue",hover_color="purple",command=send)
-        send_button.grid(row=7, column=0, columnspan=2, pady=10)
+        attachment_entry = ctk.CTkEntry(root, width=500, border_color="blue", text_color="red", font=("Latin Modern Mono Slanted", 16))
+        attachment_entry.grid(row=6, column=1, sticky="w")
 
+        attachment_button = ctk.CTkButton(root, width=100, border_color="red", hover_color="black", border_width=3, fg_color="blue", text="📎", command=select_attachment)
+        attachment_button.grid(row=6, column=1, sticky="e")
 
+        send_button = ctk.CTkButton(root, text="Send Email", font=("MathJax_Main", 16, "italic"), fg_color="blue", hover_color="purple", command=send)
+        send_button.grid(row=8, column=0, columnspan=2, pady=10)
 
-
-
-
-        
-        # Add TLS toggle
-        tls_var = ctk.StringVar(value="no")  # Default to TLS on
+        tls_var = ctk.StringVar(value="no")
         tls_checkbutton = ctk.CTkCheckBox(root, text="Use TLS", variable=tls_var, onvalue="yes", offvalue="no")
-        tls_checkbutton.grid(row=6, column=1, sticky="w")
+        tls_checkbutton.grid(row=7, column=1, sticky="w")
 
-    
+        edit_button = ctk.CTkButton(root, text="Edit Config", text_color="black", font=("MathJax_Main", 16, "italic"), fg_color="red", hover_color="green", command=edit_config)
+        edit_button.grid(row=7, column=0, columnspan=2, pady=10, sticky="se")
 
-        # Button to edit config
-        edit_button = ctk.CTkButton(root, text="Edit Config",text_color="black", font=("MathJax_Main", 16, "italic"),fg_color="red",hover_color="green", command=edit_config)
-        edit_button.grid(row=6, column=0, columnspan=2, pady=10,sticky="se")
-    
         root.mainloop()
     else:
         CTkMessagebox(title="Error", message="Failed to load configuration.", icon="warning", option_1="ok")
